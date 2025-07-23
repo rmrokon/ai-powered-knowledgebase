aadb05d9d7135199736c879d3964205ead080d40
Md Roknuzzaman <rmrokon@macbooks-MacBook-Pro.local>
2025-07-24 01:19:48 +0600

    authenticated and unauthenticated layout file added

--- frontend/src/app/(protected)/layout.tsx
+++ frontend/src/app/(protected)/layout.tsx
@@ -4,7 +4,7 @@ import { useRouter } from "next/navigation";
 
 export default function UnauthenticatedLayout({children}: {children: React.ReactNode;}){
     const token = localStorage.getItem("accessToken");
-    const router = useRouter()
+    const router = useRouter();
     if(!token){
         router.push("/login");
         return;
